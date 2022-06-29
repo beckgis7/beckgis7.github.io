8638df09e62f50ddc9b1458b4b0de877b6f01f22
Evans Ofori <evans.ofori@txtghana.com>
2022-06-29 23:42:03 +0000

    init portfolio

---
+++ src/components/Address.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Address = () => {
+  return (
+    <>
+      <p className="open-sans-font custom-span-contact position-relative">
+        <i className="fa fa-map position-absolute"></i>
+        <span className="d-block">Home Address</span>AP4 Liberty Road Adentan,
+        Ghana - West Africa.
+      </p>
+      {/* End .custom-span-contact */}
+
+      <p className="open-sans-font custom-span-contact position-relative">
+        <i className="fa fa-envelope-open position-absolute"></i>
+        <span className="d-block">mail me</span>{" "}
+        <a href="mailto:steve@mail.com">vansofori07@yahoo.com</a>
+      </p>
+      {/* End .custom-span-contact */}
+
+      <p className="open-sans-font custom-span-contact position-relative">
+        <i className="fa fa-phone-square position-absolute"></i>
+        <span className="d-block">call me</span>{" "}
+        <a href="Tel: +233 24 311 2825">+233 24 311 2825</a>
+      </p>
+      {/* End .custom-span-contact */}
+    </>
+  );
+};
+
+export default Address;
---
+++ src/components/about/PersonalInfo.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const personalInfoContent = [
+  { meta: "first name", metaInfo: "Evans" },
+  { meta: "last name", metaInfo: "Ofori" },
+  // { meta: "Age", metaInfo: "27 Years" },
+  { meta: "Nationality", metaInfo: "Ghanaian" },
+  // { meta: "Freelance", metaInfo: "Available" },
+  { meta: "Address", metaInfo: "Adentan" },
+  { meta: "phone", metaInfo: "++233243112825" },
+  { meta: "Email", metaInfo: "vansofori07@yahoo.com" },
+  { meta: "Skype", metaInfo: "live:vansofori07" },
+  { meta: "langages", metaInfo: "English" },
+];
+
+const PersonalInfo = () => {
+  return (
+    <ul className="about-list list-unstyled open-sans-font">
+      {personalInfoContent.map((val, i) => (
+        <li key={i}>
+          <span className="title">{val.meta}: </span>
+          <span className="value d-block d-sm-inline-block d-lg-block d-xl-inline-block">
+            {val.metaInfo}
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default PersonalInfo;
